6f13e769fb385c2a85448c6b73f2bd8e3be370d6
AbhishekThings <107426102+AbhishekThings@users.noreply.github.com>
2022-12-08 13:27:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,18 @@
-provider "aws" {
-region = "us-east-1"
-access_key = "proivde your user access key here"
-secret_key = "Provide your user secret key here"
-}
-
 resource "aws_instance" "one" {
-ami = "ami-0b0dcb5067f052a63"
-instance_type = var.abc
+ami = var.abc
+instance_type = var.pqr
 tags = {
-Name = "Linux Server"
-Environment = "dev"
+Name = "give the name"
+Environment = "give the environment name"
 }
 }
 
-resource "aws_instance" "two" {
-ami = "ami-0574da719dca65348"
-instance_type = var.xyz
-tags = {
-Name = "ubuntu Server"
-Environment = "test"
-}
+resource "aws_iam_user" "two" {
+ name = var.xyz
 }
 
-variable "abc" {
-description = "Hi this is my linux server"
-type = string
-default = "t2.micro"
+resource "aws_iam_group" "three" {
+ name = var.def
 }
-variable "xyz" {
-description = "Hi this is my ubuntu server"
-type = string
-default = "t2.micro"
-}
+
+
